7abbbb8e5252d3dc7d98c7665fe40e9f345bd04d
Tomasz Malicki <tmalicki81@gmail.com>
2021-08-12 16:11:00 +0200

    Tak

--- AsystentZOOM.VM/FileRepositories/BaseMediaFtpFileRepository.cs
+++ AsystentZOOM.VM/FileRepositories/BaseMediaFtpFileRepository.cs
@@ -1,14 +1,20 @@
 ﻿using FileService.Clients;
 using FileService.FileRepositories;
+using System;
 
 namespace AsystentZOOM.VM.FileRepositories
 {
     public abstract class BaseMediaFtpFileRepository : BaseFtpFileRepository
     {
+        private bool IsTmalicki
+            => Environment.MachineName.Contains("MALICKI");
+
         public override FtpSessionInfo SessionInfo => new FtpSessionInfo
         {
             HostName = "av-labedy.pl",
-            UserName = "tmalicki81-asystent-zoom-documents",
+            UserName = IsTmalicki 
+                ? "tmalicki81-asystent-zoom-documents"
+                : "tmalicki81-asystent-zoom-documents-2",
             Password = FileService.Common.CryptoHelper.DecryptString("992545eca56942ad", "BqdMmA9ox8m2XKGJNBXx0w=="),
             RemoteDirectory = RemoteDirectory
         };
